d9fa5d1e8311efc91d8fcf43748fc10c1f88c487
Alfonso Delgado <alfonso.delgado@truehome.com.mx>
2023-01-11 16:44:51 -0600

    Checking husky pre-commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express, Response, Request} from 'express';
+import express, { Express, Response, Request } from 'express';
 import helmet from 'helmet';
 
 const app: Express = express();
@@ -7,7 +7,10 @@ const port = process.env.PORT || 8080;
 app.use(helmet.hidePoweredBy());
 
 app.get('/', (_req: Request, res: Response) => {
-    res.json({ msg: 'Welcome to Xmen API Challenge'});
+  res.json({
+    title: 'Mi Compa Challenge - Xmen API',
+    version: '1.0.0'
+  });
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
