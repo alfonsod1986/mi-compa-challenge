520deda7e07413700f2e063e093460c2c4875278
Luis Alfonso Delgado Ballado <alfonsod18@gmail.com>
2023-01-12 18:08:32 -0600

    Adding Logger (#16)
    Co-authored-by: Alfonso Delgado <alfonso.delgado@truehome.com.mx>

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,3 @@ function handleError(e: any) {
   console.log(e);
   process.exit(1);
 }
-/*app.get('/', (_req: Request, res: Response) => {
-  res.json({
-    title: 'Mi Compa Challenge - Xmen API',
-    version: '1.0.0'
-  });
-});*/
